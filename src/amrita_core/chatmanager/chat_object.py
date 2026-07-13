61434aaaed0e9f5153ad42f699dfc79de411af44
RAX4096 <windowserror@163.com>
2026-07-13 21:05:30 +0800

    Fix: missed EOF markup

--- src/amrita_core/chatmanager/chat_object.py
+++ src/amrita_core/chatmanager/chat_object.py
@@ -577,6 +577,7 @@ class ChatObject:
             finally:
                 self._is_running = False
                 self._is_done = True
+                await self.io_stream.set_queue_done()  # Write a EOF to the queue
                 self.end_at = datetime.now(utc)
                 self._chatman.running_chat_object_id2map.pop(self.stream_id, None)  # type: ignore[arg-type]
                 if self._chatman.clean_obj(
